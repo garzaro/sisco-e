1eaaf70960507b904ecdc57a00fad4f4659d6dd1
garzaro <clebergarzaro74@gmail.com>
2024-06-03 23:33:36 -0300

    correcao de nomes de metodo

--- src/main/java/com/sisco/escola/service/UsuarioService.java
+++ src/main/java/com/sisco/escola/service/UsuarioService.java
@@ -1,18 +1,17 @@
 package com.sisco.escola.service;
 
 import com.sisco.escola.model.entity.Usuario;
-import com.sisco.escola.model.repository.UsuarioRepository;
 
 public interface UsuarioService {
     
-    /*verificar se o usuario existe na base*/
-    Usuario metodoParaValidarUsuario(String emailLogin, String senhalogin);
+    /*verificar se o usuario existe na base, validação*/
+    Usuario validarLogin(String emailLogin, String senhalogin);
     
     /*salvar o usuario na base*/
-    Usuario metodoParaPersistirUsuarioNaBaseDeDados(Usuario usuario);
+    Usuario persistirUsuarioNaBaseDeDados(Usuario usuario);
     
-    /*email unique*/
-    void metodoParaValidarEmailDeLoginNaBaseDeDados(String emailLogin);
+    /*verifica o email na base de dados, unique */
+    void validarEmailLoginNaBaseDeDados(String emailLogin);
     
     
     
