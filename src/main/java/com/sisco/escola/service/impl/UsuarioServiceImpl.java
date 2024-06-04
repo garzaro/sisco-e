f4b38fcab07a53e9f76f84244a7dfa2359a2ca80
garzaro <clebergarzaro74@gmail.com>
2024-06-03 23:34:34 -0300

    validar login e salvar usuario

--- src/main/java/com/sisco/escola/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/sisco/escola/service/impl/UsuarioServiceImpl.java
@@ -1,9 +1,12 @@
 package com.sisco.escola.service.impl;
 
+import com.sisco.escola.exception.ErroDeAutenticacao;
 import com.sisco.escola.exception.RegraDeNegocioException;
 import com.sisco.escola.model.entity.Usuario;
 import com.sisco.escola.model.repository.UsuarioRepository;
 import com.sisco.escola.service.UsuarioService;
+
+import java.util.Optional;
 
 public class UsuarioServiceImpl implements UsuarioService {
     
@@ -14,22 +17,34 @@ public class UsuarioServiceImpl implements UsuarioService {
         this.usuarioRepository = usuarioRepository;
     }
     
+    /*login: validação, autenticação*/
+    
     @Override
-    public Usuario metodoParaValidarUsuario(String emailLogin, String senhalogin) {
-        return null;
+    public Usuario validarLogin(String emailLogin, String senhalogin) {
+        Optional<Usuario> validandoLogin = usuarioRepository.findByEmailLogin(emailLogin);
+        /*verificar a existencia de usuario na base de dados*/
+        if(!validandoLogin.isPresent()){
+            throw new ErroDeAutenticacao("Usuario não encontrado");
+        }
+        if (!validandoLogin.get().getSenhaLogin().equals(senhalogin)){
+            throw new ErroDeAutenticacao("Digite a senha correta");
+        }
+        return validandoLogin.get();
     }
     
     @Override
-    public Usuario metodoParaPersistirUsuarioNaBaseDeDados(Usuario usuario) {
-        return null;
+    public Usuario persistirUsuarioNaBaseDeDados(Usuario usuarioLogin) {
+        /*service*/
+        validarEmailLoginNaBaseDeDados(usuarioLogin.getEmailLogin());
+        return usuarioRepository.save(usuarioLogin);
     }
     
     @Override
-    public void metodoParaValidarEmailDeLoginNaBaseDeDados(String emailLogin) {
-        /*ver se existe*/
-        boolean consultarSeEmailLoginExisteNaBaseDeDados = usuarioRepository.existsByEmailLogin(emailLogin);
-        if (consultarSeEmailLoginExisteNaBaseDeDados){
-            throw new RegraDeNegocioException("Um calango ou uma calanga ja usou esse email.");
+    public void validarEmailLoginNaBaseDeDados(String emailLogin) {
+        /*ver se existe email*/
+        boolean verificarSeOEmailLoginExisteNaBaseDeDados = usuarioRepository.existsByEmailLogin(emailLogin);
+        if (verificarSeOEmailLoginExisteNaBaseDeDados){
+            throw new RegraDeNegocioException("Ja existe um usuario com esse email.");
         }
     
     }
