4ce2f1d823d053210ad3f30733e56eaaa69bcbea
garzaro <clebergarzaro74@gmail.com>
2024-06-06 17:51:48 -0300

    ajustes

--- src/main/java/com/sisco/escola/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/sisco/escola/service/impl/UsuarioServiceImpl.java
@@ -5,24 +5,23 @@ import com.sisco.escola.exception.RegraDeNegocioException;
 import com.sisco.escola.model.entity.Usuario;
 import com.sisco.escola.model.repository.UsuarioRepository;
 import com.sisco.escola.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Optional;
 
 public class UsuarioServiceImpl implements UsuarioService {
-    
+    @Autowired
     UsuarioRepository usuarioRepository;
-    
-    public UsuarioServiceImpl(UsuarioRepository usuarioRepository) {
-        super();
-        this.usuarioRepository = usuarioRepository;
-    }
-    
-    /*login: validação, autenticação*/
-    
-    @Override
-    public Usuario validarLogin(String emailLogin, String senhalogin) {
-        Optional<Usuario> validandoLogin = usuarioRepository.findByEmailLogin(emailLogin);
-        /*verificar a existencia de usuario na base de dados*/
+   
+public UsuarioServiceImpl(UsuarioRepository usuarioRepository) {
+    super();
+    this.usuarioRepository = usuarioRepository;
+}
+/*login: validação, autenticação*/
+@Override
+public Usuario validarLogin(String emailLogin, String senhalogin) {
+    Optional<Usuario> validandoLogin = usuarioRepository.findByEmailLogin(emailLogin);
+    /*verificar a existencia de usuario na base de dados*/
         if(!validandoLogin.isPresent()){
             throw new ErroDeAutenticacao("Usuario não encontrado");
         }
@@ -46,6 +45,5 @@ public class UsuarioServiceImpl implements UsuarioService {
         if (verificarSeOEmailLoginExisteNaBaseDeDados){
             throw new RegraDeNegocioException("Ja existe um usuario com esse email.");
         }
-    
     }
 }
