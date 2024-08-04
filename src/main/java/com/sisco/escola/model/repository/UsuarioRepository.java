28b7a93e5be9a57125c7e0607e8948ae2cd68046
garzaro <clebergarzaro74@gmail.com>
2024-08-04 12:16:45 -0300

    Optional de usuario

--- src/main/java/com/sisco/escola/model/repository/UsuarioRepository.java
+++ src/main/java/com/sisco/escola/model/repository/UsuarioRepository.java
@@ -2,10 +2,9 @@ package com.sisco.escola.model.repository;
 
 import com.sisco.escola.model.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-@Repository
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
     
     /*existe um usuario com um email*/
