d0bcf52468bcbf2c2997da19d7d9cbe84cf25481
garzaro <clebergarzaro74@gmail.com>
2024-08-27 21:55:16 -0300

    nome de variavel renomeado

--- src/main/java/com/sisco/escola/exception/RegraDeNegocioException.java
+++ src/main/java/com/sisco/escola/exception/RegraDeNegocioException.java
@@ -2,8 +2,8 @@ package com.sisco.escola.exception;
 
 public class RegraDeNegocioException extends RuntimeException{
     
-    public RegraDeNegocioException(String mensagemAvisandoQueJaExisteUsuarioPeloEmailInformado) {
+    public RegraDeNegocioException(String escolaJaCadastrada) {
         
-        super(mensagemAvisandoQueJaExisteUsuarioPeloEmailInformado);
+        super(escolaJaCadastrada);
     }
 }
