ee84829cdee13d1ca7eb7e118e6fe92647288f1c
garzaro <clebergarzaro74@gmail.com>
2024-08-27 22:06:15 -0300

    ajustes - existsByCadastroEscola

--- src/main/java/com/sisco/escola/model/repository/EscolaRepository.java
+++ src/main/java/com/sisco/escola/model/repository/EscolaRepository.java
@@ -1,12 +1,19 @@
 package com.sisco.escola.model.repository;
 
 import com.sisco.escola.model.entity.Escola;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EscolaRepository extends JpaRepository<Escola, Long> {
     
+    /*verifica a existencia de uma escola no banco de dados*/
     boolean existsByNomeEscola(String nomeEscola);
-    boolean existsByCidadeEscola(String nomeEscola);
-    boolean existsByBairroEscola(String nomeEscola);
+    boolean existsByCadastroEscola(String cadastroEscola);
     
+    /*procura uma escola pelo nome no banco de dados*/
+    Optional<Escola> findByNomeEscolaOrCadastroEscola(String nomeEscola, String cadastroEscola);
+    
+    Escola findByNomeEscola(String nomeEscola);
 }
