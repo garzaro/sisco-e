27ac2af72d081c5f057a13dbaaf4341a3f35d277
garzaro <clebergarzaro74@gmail.com>
2024-08-28 22:37:06 -0300

    ajustes

--- src/main/java/com/sisco/escola/model/repository/UsuarioRepository.java
+++ src/main/java/com/sisco/escola/model/repository/UsuarioRepository.java
@@ -2,14 +2,15 @@ package com.sisco.escola.model.repository;
 
 import com.sisco.escola.model.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
     
     /*existe um usuario com um email*/
     boolean existsByEmailLogin(String emailLogin);
-    
     /*busca um usuario por email*/
     Optional<Usuario> findByEmailLogin(String emailLogin);
+    Optional<Usuario> findById(Long id);
 }
