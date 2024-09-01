0dc91f1faf1cc2fe2b4450da07736e2b9f16225b
garzaro <clebergarzaro74@gmail.com>
2024-09-01 11:23:13 -0300

    metodo existsByCpf

--- src/main/java/com/sisco/escola/model/repository/UsuarioRepository.java
+++ src/main/java/com/sisco/escola/model/repository/UsuarioRepository.java
@@ -6,11 +6,15 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     
     /*existe um usuario com um email*/
-    boolean existsByEmailLogin(String emailLogin);
+    boolean existsByEmail(String email);
+    
+    /*existe um usuario com um cpf*/
+    boolean existsByCpf(String cpf);
+    
     /*busca um usuario por email*/
-    Optional<Usuario> findByEmailLogin(String emailLogin);
-    Optional<Usuario> findById(Long id);
+    Optional<Usuario> findByEmail(String email);
+    
 }
