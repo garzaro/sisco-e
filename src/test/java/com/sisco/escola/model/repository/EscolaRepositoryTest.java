743333555ab75cdc5d376bf67ed70700361d79b0
garzaro <clebergarzaro74@gmail.com>
2024-09-02 19:15:43 -0300

    ajustes

--- src/test/java/com/sisco/escola/model/repository/EscolaRepositoryTest.java
+++ src/test/java/com/sisco/escola/model/repository/EscolaRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.Optional;
+
 @SpringBootTest
 @ActiveProfiles("test")
 public class EscolaRepositoryTest {
@@ -26,10 +28,10 @@ public class EscolaRepositoryTest {
         escolaRepository.save(escolaDeTeste);
         
         /*AÇÃO*/
-        boolean verificarSeExisteAEscola = escolaRepository.findByNomeEscola("Marvin");
+        Optional<Escola> verificarSeExisteAEscola = escolaRepository.findByNomeEscola("Marvin");
         
         /*VERIFICAÇÃO*/
-        Assertions.assertThat(verificarSeExisteAEscola).isTrue();
+        Assertions.assertThat(verificarSeExisteAEscola).isPresent();
         
     }
 }
