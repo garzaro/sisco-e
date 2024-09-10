2c22a600951b00dab66940b4c5d5c559cdcc622a
garzaro <clebergarzaro74@gmail.com>
2024-09-09 23:30:28 -0300

    dto escola adiconado

---
+++ src/main/java/com/sisco/escola/api/dto/EscolaDTO.java
@@ -0,0 +1,12 @@
+package com.sisco.escola.api.dto;
+
+public class EscolaDTO {
+    
+    private String nomeEscola;
+    private String cadastroEscola;
+    private String cidadeEscola;
+    private String bairroEscola;
+    private String endereco;
+    private Integer telefone;
+    
+}
