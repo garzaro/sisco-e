0a5267c204210ef4679feb2b1ffe9420ede512ac
garzaro <clebergarzaro74@gmail.com>
2024-09-16 13:03:57 -0300

    exclusão

--- src/main/java/com/sisco/escola/exception/CodigoJaCadastradoException.java
+++
@@ -1,7 +0,0 @@
-package com.sisco.escola.exception;
-
-public class CodigoJaCadastradoException extends RuntimeException{
-    public CodigoJaCadastradoException(String erroCodigo){
-        super(erroCodigo);
-    }
-}
