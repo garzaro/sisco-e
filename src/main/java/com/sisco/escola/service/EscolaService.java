71005714481d97b9b7c1661bf241901754dfec1c
garzaro <clebergarzaro74@gmail.com>
2024-09-18 00:52:57 -0300

    obter escola por id

--- src/main/java/com/sisco/escola/service/EscolaService.java
+++ src/main/java/com/sisco/escola/service/EscolaService.java
@@ -5,12 +5,18 @@ package com.sisco.escola.service;
 import com.sisco.escola.model.entity.Escola;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public interface EscolaService {
-    /*salvar a escola na base*/
+    /*recebe uma escola e salva na base*/
     Escola salvar(Escola escola);
-    /*preencher os campos ao cadastrar escola*/
+    /*atualizar escola*/
+    Escola atualizar(Escola escola);
+    /*validar escola antes de salvar, unique*/
     void validarEscola(Escola escola);
+    /*procura uma escola pelo id*/
+    Optional<Escola> obterEscolaPorId(Long id);
 }
 
 
