143a5e93eb6dc0ee3abbf2aface39072814a03b0
garzaro <clebergarzaro74@gmail.com>
2025-04-09 14:57:54 -0300

    cors

--- src/main/java/com/sisco/escola/SiscoEApplication.java
+++ src/main/java/com/sisco/escola/SiscoEApplication.java
@@ -1,20 +1,20 @@
 package com.sisco.escola;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @SpringBootApplication
 @EnableWebMvc
 public class SiscoEApplication implements WebMvcConfigurer {
+	/*cors*/
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		WebMvcConfigurer.super.addCorsMappings(registry);
 		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
 	}
-
+	/*main*/
 	public static void main(String[] args) {
 		SpringApplication.run(SiscoEApplication.class, args);
 	}
