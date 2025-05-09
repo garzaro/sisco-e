eb714a18fe5314a8fe5ea37cd3e31adf5a9b8139
garzaro <clebergarzaro74@gmail.com>
2025-05-09 16:45:27 -0300

    retirando cors

--- src/main/java/com/sisco/escola/SiscoEApplication.java
+++ src/main/java/com/sisco/escola/SiscoEApplication.java
@@ -2,13 +2,9 @@ package com.sisco.escola;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 
 @SpringBootApplication
-public class SiscoEApplication implements WebMvcConfigurer {
+public class SiscoEApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SiscoEApplication.class, args);
 	}
