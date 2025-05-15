4e9d021b42b0a5eea7d9355d98cd52e22bc2004a
garzaro <clebergarzaro74@gmail.com>
2025-05-15 15:32:25 -0300

    cors

--- src/main/java/com/sisco/escola/config/CorsConfig.java
+++ src/main/java/com/sisco/escola/config/CorsConfig.java
@@ -14,8 +14,9 @@ public class CorsConfig {
 			public void addCorsMappings(CorsRegistry registry){
 				registry.addMapping("/**")
 				.allowedOrigins("http://localhost:3000")
-				.allowedMethods("POST", "PUT", "GET", "DELETE", "OPTIONS")
-				.allowedHeaders("*");
+				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+				.allowedHeaders("*")
+				.allowCredentials(true);
 			}
 		};
 	}
