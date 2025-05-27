73f7452a246975a63b300340e0e8302df20f0853
garzaro <clebergarzaro74@gmail.com>
2025-05-27 12:49:58 -0300

    correcao

--- src/main/java/com/sisco/escola/config/SecurityConfig.java
+++ src/main/java/com/sisco/escola/config/SecurityConfig.java
@@ -1,19 +1,7 @@
 /*package com.sisco.escola.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@EnableWebSecurity
-@Configuration
 public class SecurityConfig {
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.formLogin(configurer -> configurer.loginPage("/login").successForwardUrl("/home"));
-		return http.build();
-						
-	}
+
+   
 }
 */
