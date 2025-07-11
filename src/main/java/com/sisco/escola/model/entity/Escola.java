cb2bf9dfee58a2d38f37ed332cd65451a79caa2b
garzaro <clebergarzaro74@gmail.com>
2025-07-11 13:51:10 -0300

    retirando prop email

--- src/main/java/com/sisco/escola/model/entity/Escola.java
+++ src/main/java/com/sisco/escola/model/entity/Escola.java
@@ -29,9 +29,6 @@ public class Escola {
     @Column(name = "codigo_escola")
     private String codigo;
 
-    @Column(name = "email")
-    private String email;
-
     @Column(name = "estado")
     private String estado;
     
