687a79f1b52a4807ced6576d8eb0eb0d05fb06fe
garzaro <clebergarzaro74@gmail.com>
2025-08-08 14:54:27 -0300

    validações diretamente nas propriedades da entidade Contato

--- src/main/java/com/sisco/escola/model/entity/Contato.java
+++ src/main/java/com/sisco/escola/model/entity/Contato.java
@@ -1,17 +1,33 @@
 package com.sisco.escola.model.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "contato", schema = "siscoescola")
 public class Contato {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name="")
-    private String type;
-    @Column(name="")
-    private String number;
-    
+
+    @NotBlank(message = "Contato é obrigatório")
+    @Column(name="telefone")
+    private String telefone;
+
     @ManyToOne
-    private Escola school;
+    @JoinColumn(name = "escola_id")
+    private Escola escola;
+
+    @NotBlank(message = "Selecione um tipo de contato")
+    @Size(max = 20, message = "Tipo de contato deve ter no máximo 20 caracteres")
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "tipo_contato", nullable = false, length = 20)
+    private TipoContato tipoContato;
 
 }
