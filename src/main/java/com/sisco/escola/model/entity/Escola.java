1bebbda2cd9a3430bcdb9e9f39de35467861eb26
garzaro <clebergarzaro74@gmail.com>
2025-09-03 10:22:01 -0300

    validações de entrada da api e banco de dados

--- src/main/java/com/sisco/escola/model/entity/Escola.java
+++ src/main/java/com/sisco/escola/model/entity/Escola.java
@@ -35,50 +35,44 @@ public class Escola {
     private Long id;
 
     @NotBlank(message = "O nome da escola é obrigatório")
-    @Size(max = 100, message = "O nome escola deve ter no máximo 100 caracteres")
-    @Column(name = "nome_escola", nullable = false, length = 100)
+    @Size(min = 3, max = 100, message = "O nome escola deve ter entre 3 e 100 caracteres")
+    @Column(name = "nome_escola", nullable = false, unique = true, length = 100)
     private String nome;
 
     @NotBlank(message = "O código da escola é obrigatóro")
-    @Size(max = 10, message = "O código escola deve ter no máximo 10 caracteres")
-    @Column(name = "codigo_escola", nullable = false, length = 10)
+    @Size(min = 10, max = 10, message = "O código escola deve ter no máximo 10 caracteres")
+    @Column(name = "codigo_escola", nullable = false, unique = true, length = 10)
     private String codigo;
 
     @NotBlank(message = "O email é obrigatório")
     @Email(message = "Email inválido")
     @Size(max = 255 , message = "O email deve ter no máximo 255 caracteres")
-    @Column(name = "email", nullable = false, length = 255)
+    @Column(name = "email", nullable = false, unique = true, length = 255)
     private String email;
 
     @NotBlank(message = "O Estado é obrigatório")
-    @Size(max = 25, message = "O Estado deve ter no máximo 25 caracteres")
+    @Size(min = 3, max = 25, message = "O Estado deve ter no máximo 25 caracteres")
     @Column(name = "estado", nullable = false, length = 25)
     private String estado;
 
-
     @NotBlank(message = "O município é obrigatório")
-    @Size(max = 100, message = "O município deve ter no máximo 100 caracteres")
+    @Size(min = 3, max = 100, message = "O município deve ter no máximo 100 caracteres")
     @Column(name = "municipio", nullable = false, length = 100)
     private String municipio;
 
     @NotBlank(message = "O bairro é obrigatório")
-    @Size(max = 100, message = "O bairro deve ter no máximo 100 caracteres")
+    @Size(min = 3, max = 100, message = "O bairro deve ter no máximo 100 caracteres")
     @Column(name = "bairro", nullable = false, length = 100)
     private String bairro;
 
     @NotBlank(message = "Rua, avenida, praça, bloco é obrigatório")
-    @Size(max = 255, message = "O campo logradouro deve ter no máximo 255 caracteres")
+    @Size(min = 3, max = 255, message = "O campo logradouro deve ter no máximo 255 caracteres")
     @Column(name = "logradouro", nullable = false, length = 255)
     private String logradouro;
 
     @CreationTimestamp
-    @Column(name = "data_cadastro", updatable = false, columnDefinition = "DATE DEFAULT CURRENT_DATE")
-    private LocalDateTime dataCadastro;
-
-    /*data*/
-    /*@CreationTimestamp
     @Column(name = "data_cadastro", updatable = false)
-    private LocalDateTime dataCadastro;*/
+    private LocalDateTime dataCadastro;
     
     /*@ManyToOne
     @JoinColumn(name = "id_usuario")
