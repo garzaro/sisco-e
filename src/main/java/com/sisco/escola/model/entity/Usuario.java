5667c1357c73c47849a4247b836c3097292b01e5
garzaro <clebergarzaro74@gmail.com>
2025-09-03 10:14:41 -0300

    validações de entrada da api e banco de dados

--- src/main/java/com/sisco/escola/model/entity/Usuario.java
+++ src/main/java/com/sisco/escola/model/entity/Usuario.java
@@ -39,27 +39,28 @@ public class Usuario{
     @NotBlank(message = "O CPF é obrigatório")
     //@Pattern(regexp = "\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}$")
     //@CPF(message = "CPF inválido")
+    @Size(min = 14) //nao precisa mas coloquei, o sistem é meu
     @Column(name = "cpf", nullable = false, length = 14, unique = true)
     private String cpf;
 
     @NotBlank(message = "O nome de usuário é obrigatório")
-    @Size(max = 100, message = "O nome de usuário deve ter no máximo 100 caracteres")
-    @Column(name = "nome_usuario")
+    @Size(min = 3, max = 100, message = "O nome de usuário deve ter entre 3 e 100 100 caracteres")
+    @Column(name = "nome_usuario", nullable = false, length = 100)
     private String usuario;
 
     @NotBlank(message = "O e-mail é obrigatório")
     @Email(message = "E-mail inválido")
-    //@Pattern(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "Formato de e-mail inválido")
-    @Column(name = "email")
+    @Pattern(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "Formato de e-mail inválido")
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
     @NotBlank(message = "A senha é obrigatório")
-    @Size(max = 255, message = "Senha inválida")
-    @Column(name = "senha", nullable = false,length = 255)
+    @Size(min = 6, max = 255, message = "Senha inválida")
+    @Column(name = "senha", nullable = false, unique = true, length = 255) // ver força de senha
     private String senha;
 
     @CreationTimestamp
-    @Column(name = "data_cadastro", updatable = false, columnDefinition = "DATE DEFAULT CURRENT_DATE")
+    @Column(name = "data_cadastro", updatable = false)
     private LocalDateTime dataCadastro;
 
     /*GETTERS AND SETTERS*/
