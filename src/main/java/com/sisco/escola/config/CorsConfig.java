4883d8eb4c0ed966324c04a7568b830fd4fba812
garzaro <clebergarzaro74@gmail.com>
2025-09-04 16:22:59 -0300

    reconfigurando as origens para interações

--- src/main/java/com/sisco/escola/config/CorsConfig.java
+++ src/main/java/com/sisco/escola/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.sisco.escola.config;
 
+import jakarta.validation.constraints.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,15 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry){
-				registry.addMapping("/**")
+	WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+			public void addCorsMappings(@NotNull CorsRegistry registry){
+				registry.addMapping("/api/usuarios/**")
 				.allowedOrigins("http://localhost:3000")
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 				.allowedHeaders("*")
-				.allowCredentials(true);
+				.allowCredentials(true)
+                .maxAge(3600);
 			}
 		};
 	}
