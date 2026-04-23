8dcafb3edc3f42669bb40bc053869ec3bba0dc9e
garzaro <clebergarzaro74@gmail.com>
2026-04-23 00:52:43 -0300

    Alterando versao do java para 21

--- src/main/java/com/sisco/escola/validacao/CpfValidador.java
+++ src/main/java/com/sisco/escola/validacao/CpfValidador.java
@@ -2,79 +2,8 @@ package com.sisco.escola.validacao;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import java.util.InputMismatchException;
 
-//Implementacao retirada de: https://www.devmedia.com.br/validacao-de-cpf-em-java/4110
-
-//public class CpfValidador implements ConstraintValidator<CpfValido, String> {
-//
-//    @Override
-//    public void initialize(CpfValido constraintAnnotation) {
-//    }
-//
-//    @Override
-//    public boolean isValid(String cpf, ConstraintValidatorContext context) {
-//        if (cpf == null || cpf.trim().isEmpty()) {
-//            return false;
-//        }
-//
-//        cpf = cpf.replaceAll("\\D", "");
-//
-//        if (cpf.length() != 11) {
-//            return false;
-//        }
-//
-//        // Verifica CPFs com números iguais (ex: 111.111.111-11)
-//        if (cpf.matches("(\\d)\\1{10}")) {
-//            return false;
-//        }
-//
-//        try {
-//            char dig10, dig11;
-//            int sm, i, r, num, peso;
-//
-//            // Calculo do 1o. Digito Verificador
-//            sm = 0;
-//            peso = 10;
-//            for (i = 0; i < 9; i++) {
-//                num = (int) (cpf.charAt(i) - 48);
-//                sm = sm + (num * peso);
-//                peso = peso - 1;
-//            }
-//
-//            r = 11 - (sm % 11);
-//            if ((r == 10) || (r == 11)) {
-//                dig10 = '0';
-//            } else {
-//                dig10 = (char) (r + 48);
-//            }
-//
-//            // Calculo do 2o. Digito Verificador
-//            sm = 0;
-//            peso = 11;
-//            for (i = 0; i < 10; i++) {
-//                num = (int) (cpf.charAt(i) - 48);
-//                sm = sm + (num * peso);
-//                peso = peso - 1;
-//            }
-//
-//            r = 11 - (sm % 11);
-//            if ((r == 10) || (r == 11)) {
-//                dig11 = '0';
-//            } else {
-//                dig11 = (char) (r + 48);
-//            }
-//
-//            // Verifica se os digitos calculados conferem com os digitos informados.
-//            return (dig10 == cpf.charAt(9)) && (dig11 == cpf.charAt(10));
-//        } catch (InputMismatchException erro) {
-//            return false;
-//        }
-//    }
-//}
-
-public class CpfValidador implements ConstraintValidator<CpfValido, String> {
-
+public class CpfValidador  implements ConstraintValidator<CpfValido, String> {
     @Override
     public boolean isValid(String cpf, ConstraintValidatorContext context) {
         if (cpf == null || cpf.trim().isEmpty()) return false;
@@ -100,4 +29,3 @@ public class CpfValidador implements ConstraintValidator<CpfValido, String> {
         return (resto > 9) ? 0 : resto;
     }
 }
-
--- src/main/java/com/sisco/escola/validacao/CpfValido.java
+++ src/main/java/com/sisco/escola/validacao/CpfValido.java
@@ -1,15 +1,12 @@
 package com.sisco.escola.validacao;
 
 import jakarta.validation.Constraint;
-import jakarta.validation.Payload;
+
 import java.lang.annotation.*;
 
 @Documented
 @Constraint(validatedBy = CpfValidador.class)
 @Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface CpfValido {
-    String message() default "{usuario.cpf.invalid}";
-    Class<?>[] groups() default {};
-    Class<? extends Payload>[] payload() default {};
+public @interface  CpfValido {
 }
