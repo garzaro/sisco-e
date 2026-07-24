f613d7c89d62620ee1057fb94a5493ac7de81c51
garzaro <clebergarzaro74@gmail.com>
2026-07-24 13:36:17 -0300

    continuação

--- src/main/java/com/sisco_e/escola/model/entity/Usuario.java
+++ src/main/java/com/sisco_e/escola/model/entity/Usuario.java
@@ -1,35 +1,31 @@
 package com.sisco_e.escola.model.entity;
 
 import java.time.Instant;
-import java.util.Set;
 import java.util.UUID;
-import java.util.HashSet;
-
 import org.hibernate.annotations.CreationTimestamp;
 
-import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.envers.Audited;
 
-@Builder
-@NoArgsConstructor
+@Audited
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
+@Builder
 @Entity
-@Table(name = "usuario",  schema = "escola")
+@Table(name = "usuario",  schema = "siscoescola")
 public class Usuario {
 	@Id
 	@GeneratedValue(strategy = GenerationType.UUID)
@@ -40,28 +36,33 @@ public class Usuario {
     private String nomeCompleto;
     	
     @Column(name = "usuario", nullable = false, columnDefinition = "CHARACTER VARYING(120)")
-    private String usuario;
+    private String username;
 
-    @Column(name = "cpf", unique = true, columnDefinition = "CHARACTER VARYING(14)")
+    @Column(name = "cpf", unique = true, columnDefinition = "CHARACTER VARYING(11)")
 	private String cpf;	
 	
 	@Column(name = "email", nullable = false, unique = true, columnDefinition = "CHARACTER VARYING(120)")
 	private String email;
 	
 	@Column(name = "senha", nullable = false, columnDefinition = "CHARACTER VARYING(255)")
-	private String senha;
+	private String password;
 	
 	@CreationTimestamp
     @Column(nullable = false, updatable = false)
 	private Instant dataCadastro;
-	
-	@Column(name = "ativo")
-    private Boolean ativo;	
 
-	@ElementCollection(fetch = FetchType.EAGER)
-	@CollectionTable(name = "usuario_roles", schema = "escola", joinColumns = @JoinColumn(name = "usuario_id"))
-	@Column(name = "role")
-	@Builder.Default
-	private Set<String> roles = new HashSet<>();
+	@UpdateTimestamp
+	@Column(nullable = false, updatable = false)
+	private Instant dataAtualizacao;
+
+	@Column(name = "is_ativo")
+    private Boolean isAtivo;
+
+//	@ElementCollection(fetch = FetchType.EAGER)
+//	@CollectionTable(name = "usuario_roles", schema = "escola", joinColumns = @JoinColumn(name = "usuario_id"))
+//	@Column(name = "role")
+//	@Builder.Default
+//	private Set<String> roles = new HashSet<>();
+	
 
 }
